6f709ba7ba0d9452430c4a165325b8787f3f12ce
Aek <ekkawin.v@gmail.com>
2020-12-27 01:11:13 +0700

    model 1

--- index.ts
+++ index.ts
@@ -1,21 +1,35 @@
 const express = require('express');
 const axios = require('axios');
+const redis = require('redis');
 
 const app = express();
 const PORT = 9000;
+
+const redisClient = redis.createClient();
+redisClient.on('error', function (error) {
+  console.error(error);
+});
 
 app.get('/', async (req, res) => {
   const userName = req.query.userName || 'Ekkawin';
   const url = `https://api.github.com/users/${userName}`;
 
-  const { data } = await axios.get(url);
+  redisClient.get(userName, async (err, reply) => {
+    if (reply) {
+      console.log('reply', reply);
+      return res.json(JSON.parse(reply));
+    }
+    if (err) {
+      console.error(err);
+      throw new Error('new Error');
+    }
+    const { data } = await axios.get(url);
+    redisClient.setex(userName, 60, JSON.stringify(data));
+    return res.json(data);
+  });
+
   console.log('userName', userName);
   console.log('url', url);
-  console.log('user', data);
-
-  res.json({
-    data: data,
-  });
 });
 
 app.listen(PORT, () => {
